db90558ff18659a3de2de03010f958f7a73c4d04
Matthew Smith <matt@beamend.com>
2021-06-20 22:33:50 +0100

    added intial calendar api framework

--- home.php
+++ home.php
@@ -19,8 +19,12 @@ if (isset($_GET['logout'])) {
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/vendor/fullcalendar/dist/fullcalendar.min.css">
+    <link rel="stylesheet" href="assets/vendor/sweetalert2/dist/sweetalert2.min.css">   
     <link href="./css/home.css" rel="stylesheet">
     <title>Home</title>
+    <link href='lib/calendar/lib/main.css' rel='stylesheet' />
+
 </head>
 
 <body>
@@ -41,8 +45,11 @@ if (isset($_GET['logout'])) {
     </header>
 
     <main class="container">
-        <h1 class="display-3">Welcome, <?php echo $_SESSION['firstname']; ?></h1>
+        <h1 class="display-4 text-center">Welcome, <?php echo $_SESSION['firstname']; ?></h1>
+        <div id="calendar"></div>
     </main>
+
+    
 
     <footer class="border-top footer blue text-muted">
         <div class="container text-center">
@@ -50,9 +57,20 @@ if (isset($_GET['logout'])) {
         </div>
     </footer>
 
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src='lib/calendar/lib/main.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                aspectRatio: 2,
+                themeSystem: 'boostrap',
+                initialView: 'dayGridMonth',
+                height: 650,
+            });
+            calendar.render();
+        });
+    </script>
 
 </body>
 
